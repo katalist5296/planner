9f31c80b5297809dd843d8e8b689e03eaab2a2dd
katalist52 <katalist5296@gmail.com>
2021-04-11 23:37:59 +0300

    clean architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from './core/layouts/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import { NotFoundComponent } from './layouts/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    NotFoundComponent
+  ]
+})
+export class CoreModule {}
